e57c16f6ddbfb228f383abb9c5f81db3bbd958ae
582875 <reedpalmer@C02QK0V9G8WM.local>
2016-01-28 14:36:31 -0500

    Actually deleted everything

--- build_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-# Build the images
-#./build_docker_base.sh
-./build_docker_jenkins.sh
-./build_docker_jenkins_job_builder.sh
-cd spring-petclinic
-./build_docker_petclinic.sh
-cd ..
