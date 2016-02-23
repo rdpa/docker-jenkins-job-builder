4b9f35d5d0cdccc38db50a1207e7a83ddeb719af
rdpa <rdpalmer@live.unc.edu>
2016-02-23 12:10:12 -0500

    Changed build script

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,8 +1,9 @@
 set -v
 set +e
-docker kill jjb
-docker rm jjb
-docker rmi rdpa/jenkins-job-builder
+docker kill jenkins jenkins-job-builder
+docker rm jenkins jenkins-job-builder
 set -e
-docker build -t rdpa/jenkins-job-builder docker/jenkins-job-builder/.
-docker run -d -name jjb rdpa/jenkins-job-builder
+docker build --no-cache -t rdpa/jenkins docker/jenkins/.
+docker build --no-cache -t rdpa/jenkins-job-builder docker/jenkins-job-builder/.
+docker push rdpa/jenkins
+docker push rdpa/jenkins-job-builder
